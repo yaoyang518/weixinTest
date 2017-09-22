f29d22ef7f3a302ed990cb67a2a33968718a9774
yaoyang <yaoyang518@126.com>
2017-09-22 10:04:18 +0800

    firstCommit

---
+++ src/study/Test.java
@@ -0,0 +1,10 @@
+package study;
+
+public class Test {
+
+	public static void main(String[] args) {
+		String str=String.format("%s[%s单号：%e]", "你好","2012",1.22e+5);
+		System.out.println(str);
+	}
+
+}
