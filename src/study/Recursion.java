168dab925bb0b8c2fb8d2390a5478ccb36b2744b
yaoyang <yaoyang518@126.com>
2018-08-15 21:48:13 +0800

    递归

--- src/study/Recursion.java
+++ src/study/Recursion.java
@@ -2,13 +2,6 @@ package study;
 
 public class Recursion {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		String string = "((((&&EXTPARAM_STRING(strTest)==\"a\")||((EXTPARAM_STRING(strTest)IN\"b\"!))&&";
-		getString(string);
-		System.out.println(getString(string));
-	}
-
 	public static String getString(String s) {
 		if (s.startsWith("(")) {
 			s = s.substring(1);
@@ -18,4 +11,32 @@ public class Recursion {
 		}
 	}
 
+
+	// 1.递归求和1+2+3+.....+n
+	public static int getSum(int n) {
+		if (n > 0) {
+			return n + getSum(n - 1);
+		} else {
+			return 0;
+		}
+	}
+	
+	//2.递归阶乘n! = n * (n-1) * (n-2) * ...* 1(n>0)
+	public static int getFactorial(int n) {
+		if (n > 0) {
+			return n * getFactorial(n - 1);
+		} else {
+			return 1;
+		}
+	}
+	
+
+	public static void main(String[] args) {
+		// String string =
+		// "((((&&EXTPARAM_STRING(strTest)==\"a\")||((EXTPARAM_STRING(strTest)IN\"b\"!))&&";
+		// getString(string);
+		//getSum(5);
+		
+		System.out.println(getFactorial(5));
+	}
 }
